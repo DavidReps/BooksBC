891cdd5bb945f65f4e0581554efec6dd1968a751
Manogya Acharya <acharyma@bc.edu>
2021-05-12 16:16:49 -0400

    heroku deployment stuff

--- BookExBC/settings.py
+++ BookExBC/settings.py
@@ -138,6 +138,7 @@ USE_TZ = True
 
 #IMAGESTUFF
 PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS = [
     os.path.join(PROJECT_ROOT, 'static'),
 ]
